862fe1cc6883fab147408583b54a8ff8b52591e9
hongwx <hong_wx@i-hengfeng.com>
2024-02-22 16:41:48 +0800

    feat: 添加vue3测试项目

---
+++ examples/vite-project-vue3/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+
+<style scoped></style>
---
+++ examples/vite-project-vue3/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+
+  <div>bool: {{ bool }}</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { isArray } from '@hf/shared';
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+
+const bool = isArray(count);
+</script>
+
+<style scoped></style>
